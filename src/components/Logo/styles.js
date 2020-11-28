5b6e48c19f66b49739058c217d16287919e073f4
luster02 <bombolti_02@hotmail.com>
2020-11-27 22:06:27 -0600

    [ADD]: logo component

---
+++ src/components/Logo/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Svg = styled.svg`
+  width: 220px;
+  margin-left: 15px;
+  margin-top: 15px;
+  margin-bottom:15px;
+`
+
+export const Col = styled.div`
+    display: flex;
+    justify-content:space-between;
+`
+
+export const Icon = styled.button` 
+    display: flex; 
+    align-items: center;
+    margin-right: 15px
+    /* padding: 15px; */
+`
